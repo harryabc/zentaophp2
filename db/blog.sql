2e09d9e44841769a3f6a38a9731c56145f322062
luhan <han.lu@cloudtogo.cn>
2021-06-02 10:18:49 +0800

    add

---
+++ db/blog.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS `blog`;
+CREATE DATABASE `blog`;
+USE `blog`;
+
+DROP TABLE IF EXISTS `blog`;
+CREATE TABLE IF NOT EXISTS `blog` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `title` char(120) NOT NULL,
+  `content` text NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
